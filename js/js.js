6e54e14bde80bdf85b575d352ea0ef5cba78a7c1
j104d2008 <66717407+j104d2008@users.noreply.github.com>
2026-01-29 02:33:54 +0700

    Create js.js

---
+++ js/js.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.querySelector(".menu-toggle");
+  const nav = document.querySelector(".nav");
+
+  toggle.addEventListener("click", () => {
+    nav.classList.toggle("open");
+  });
+});
